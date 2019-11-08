a06004c9f221ac6bf47a27c8dcc688f362a01aa3
Alviannn <alviandq@yahoo.com>
2019-11-09 00:39:40 +0700

    Removed unused constructor

--- src/main/java/com/github/alviannn/scheduler/SchedulerException.java
+++ src/main/java/com/github/alviannn/scheduler/SchedulerException.java
@@ -1,14 +1,10 @@
 package com.github.alviannn.scheduler;
 
-public class SchedulerException extends RuntimeException {
+class SchedulerException extends RuntimeException {
 
     static final long serialVersionUID = -2655387724036259457L;
 
-    public SchedulerException() {
-        super();
-    }
-
-    public SchedulerException(String message) {
+    SchedulerException(String message) {
         super(message);
     }
 
